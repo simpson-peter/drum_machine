941ead8d8f47f5b0bb7b9bfe75c88c37cf0319ce
simpson-peter <simpson.petere@gmail.com>
2021-04-14 11:46:45 -0500

    beginning initial layout

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0x1b1a17);
+const Color kForeGroundColor = Color(0xa35709);
+const Color kButtonColor = Color(0xf0e3ca);
+const Color kHighlightColor = Color(0xff8303);
+const int kLineMargin = 24;
---
+++ lib/machine_line.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//This widget represents one s
+class MachineLine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
