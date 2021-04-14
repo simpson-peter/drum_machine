2c10485689234aae758d734e5efc367e186e7de0
simpson-peter <simpson.petere@gmail.com>
2021-04-14 11:27:17 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/me/petersimpson/drum_machine/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.petersimpson.drum_machine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/app_data.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+//Class which tracks cross-app data, utilized by Provider ChangeNotifier instance
+class AppData extends ChangeNotifier {}
