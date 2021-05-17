242e3b9fdc2417f51929375683bef337de9f8e35
simpson-peter <simpson.petere@gmail.com>
2021-05-17 09:59:30 -0500

    first pass implementation of InstrumentReadout

--- lib/widgets/drum_machine.dart
+++ lib/widgets/drum_machine.dart
@@ -1,5 +1,6 @@
 import 'package:drum_machine/constants.dart';
 import 'package:drum_machine/widgets/beat_readout.dart';
+import 'package:drum_machine/widgets/instrument_readout.dart';
 import 'package:flutter/material.dart';
 
 class DrumMachine extends StatefulWidget {
@@ -16,6 +17,9 @@ class _DrumMachineState extends State<DrumMachine> {
         child: Column(
           children: [
             BeatReadout(),
+            InstrumentReadout(instrument: Instrument.Kick),
+            InstrumentReadout(instrument: Instrument.Snare),
+            InstrumentReadout(instrument: Instrument.Cymbal),
           ],
         ),
       ),
--- lib/widgets/instrument_readout.dart
+++ lib/widgets/instrument_readout.dart
@@ -1,21 +1,36 @@
 import 'package:drum_machine/constants.dart';
+import 'package:drum_machine/widgets/instrument_button.dart';
 import 'package:flutter/material.dart';
 
 //Widget which holds all of the instrument buttons, which represent the pattern for the given instrument
 class InstrumentReadout extends StatefulWidget {
+  final Instrument instrument;
+
+  InstrumentReadout({@required this.instrument});
   @override
   _InstrumentReadoutState createState() => _InstrumentReadoutState();
 }
 
 class _InstrumentReadoutState extends State<InstrumentReadout> {
+  List<Widget> buildInstrumentReadout() {
+    List<InstrumentButton> instrumentButtons = List.filled(kTotalBeats, null);
+    for (int i = 0; i < kTotalBeats; i++) {
+      instrumentButtons[i] = InstrumentButton(
+        beat: i + 1,
+        instrument: widget.instrument,
+      );
+    }
+    return instrumentButtons;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: kBackgroundColor,
       child: Row(
-          //TODO: Include Instrument nameplate
-
-          ),
+        //TODO: Include Instrument nameplate
+        children: buildInstrumentReadout(),
+      ),
     );
   }
 }
